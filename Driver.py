98a28a203b3368d7ab131eb018469829438941ec
Ali <ali1955.2011@gmail.com>
2025-08-07 12:29:55 +0300

    Refactor get_green_titles and get_rewards functions for improved readability and error handling; remove old output.csv file, add get_esdad_content

--- Driver.py
+++ Driver.py
@@ -5,6 +5,7 @@ import logging
 from urllib.parse import urljoin
 import re
 import pandas as pd
+from tabulate import tabulate
 
 
 class TitleNotFoundError(Exception):
@@ -34,6 +35,7 @@ def fetch_page(url: str):
         logging.error(f"Error Fetching {url} page: {e}")
         return None
 
+
 def get_title(url: str, soup: BeautifulSoup) -> str:
     title_tag = soup.select_one("h1.inner-header-title")
     if title_tag:
@@ -43,6 +45,7 @@ def get_title(url: str, soup: BeautifulSoup) -> str:
     else:
         logging.error("Title tag 'h1.inner-header-title' not found.")
         raise TitleNotFoundError("Title tag not found in HTML")
+
 
 def get_titles(url: str, soup: BeautifulSoup) -> list:
 
@@ -55,78 +58,80 @@ def get_titles(url: str, soup: BeautifulSoup) -> list:
         logging.error("Title tag 'div.staff-item--desc' not found.")
         raise TitleNotFoundError("Title tag not found in HTML")
 
+
 def get_green_titles(url: str, soup: BeautifulSoup) -> list:
-    
-  
-    
+
     data_list = []
-    
-    titles_tag = soup.find_all('span', style=re.compile(r'color:\s*#799E91'))
-    
+
+    titles_tag = soup.find_all("span", style=re.compile(r"color:\s*#799E91"))
+
     for title_span in titles_tag:
         try:
             title = title_span.get_text(strip=True)
-            
-    
+
             parent = title_span.parent
             list_elements = None
             if parent:
-                list_elements = parent.find_next_sibling(['ul', 'ol'])
-            
+                list_elements = parent.find_next_sibling(["ul", "ol"])
+
             full_text = ""
             if list_elements:
-                points = [li.get_text(strip=True) for li in list_elements.find_all('li')]#type:ignore
-                full_text = '\n'.join(points)
+                points = [
+                    li.get_text(strip=True) for li in list_elements.find_all("li")
+                ]  # type:ignore
+                full_text = "\n".join(points)
 
-            row = {
-                'url' : url,
-                'title' : title,
-                'text' : full_text
-            }
-            
+            row = {"url": url, "title": title, "text": full_text}
+
             data_list.append(row)
-            
+
         except Exception as e:
             print(f"Error occurred while processing a title: {e}")
             continue
-            
-    return data_list
-        
-def get_rewards(url:str, soup:BeautifulSoup) -> list:
-            data_list = []
-            
-            all_prizes = soup.select('div.prizes-list div.prize-item')
-            
-            if not all_prizes:
-                logging.error('Unavailable rewards')
-                raise TitleNotFoundError("Unavailable rewards")
-        
-            for prize_item in all_prizes:
-                title_tag = prize_item.select_one('div.prize-item-content div.prize-item--title')
-                title = title_tag.get_text(strip=True) if title_tag else 'Empty'
-                
-                source_tag = prize_item.select_one('div.prize-item-content div.prize-item--desc')
-                source = source_tag.get_text(strip=True) if source_tag else 'Empty'
-                
-                year_tag = prize_item.select_one('div.prize-item-icon svg text[font-size = "15"]')
-                year = year_tag.get_text(strip=True) if year_tag else 'Empty'
-                
-                month_tag = prize_item.select_one('div.prize-item-icon svg text[font-size = "14"]')
-                month = month_tag.get_text(strip=True) if month_tag else 'c'
-                
-                full_title = f"{title} - {source}"
-                full_text = f"{year} - {month}"
 
-                row = {
-                    'url' : url,
-                    'title' : full_title,
-                    'text'  : full_text
-                }
-                
-                data_list.append(row)
-                
-            return data_list
-                               
+    return data_list
+
+
+def get_rewards(url: str, soup: BeautifulSoup) -> list:
+    data_list = []
+
+    all_prizes = soup.select("div.prizes-list div.prize-item")
+
+    if not all_prizes:
+        logging.error("Unavailable rewards")
+        raise TitleNotFoundError("Unavailable rewards")
+
+    for prize_item in all_prizes:
+        title_tag = prize_item.select_one(
+            "div.prize-item-content div.prize-item--title"
+        )
+        title = title_tag.get_text(strip=True) if title_tag else "Empty"
+
+        source_tag = prize_item.select_one(
+            "div.prize-item-content div.prize-item--desc"
+        )
+        source = source_tag.get_text(strip=True) if source_tag else "Empty"
+
+        year_tag = prize_item.select_one(
+            'div.prize-item-icon svg text[font-size = "15"]'
+        )
+        year = year_tag.get_text(strip=True) if year_tag else "Empty"
+
+        month_tag = prize_item.select_one(
+            'div.prize-item-icon svg text[font-size = "14"]'
+        )
+        month = month_tag.get_text(strip=True) if month_tag else "c"
+
+        full_title = f"{title} - {source}"
+        full_text = f"{year} - {month}"
+
+        row = {"url": url, "title": full_title, "text": full_text}
+
+        data_list.append(row)
+
+    return data_list
+
+
 def get_names(url: str, soup: BeautifulSoup) -> list:
     names_tag = soup.select("div.staff-item--title")
     if names_tag:
@@ -143,6 +148,7 @@ def get_paragraph(soup: BeautifulSoup) -> str:
     ps = soup.select("div.inner-body-desc p")
     paragraphs = [p.get_text(strip=True) for p in ps]
     return "\n".join(paragraphs)
+
 
 # --- Remember to change the return hint --- #
 def about_us_content(url: str) -> dict:
@@ -173,7 +179,7 @@ def about_us_content(url: str) -> dict:
             # pages_tags = soup.select('a.page-link')
             # titles.extend(get_titles(url,soup))
             # texts.extend(get_names(url,soup))
-            
+
             # if pages_tags:
             #     for link in pages_tags:
             #         if link.has_attr('href'):
@@ -185,8 +191,8 @@ def about_us_content(url: str) -> dict:
             #                 texts.extend(get_names(new_url, new_soup))
             # for title,name in zip(titles,texts):
             #     print(f"{title}: {name}")
-                pass
-                
+            pass
+
         elif url.endswith("/sharia-supervisory-board"):
             # df = pd.DataFrame(get_green_titles(url,soup))
             # df.to_csv('output.csv', index=False, encoding='utf-8-sig')
@@ -195,16 +201,82 @@ def about_us_content(url: str) -> dict:
         # elif url.endswith("/prizes"):
         #     print(get_title(soup))
         elif url.endswith("/content/prizes"):
-            get_rewards(url,soup)
-            print('test')
-            
+            get_rewards(url, soup)
+            print("test")
+
     except TitleNotFoundError as e:
         print(e)
         return {}
+
     return {"url": url, "title": titles, "text": texts}
 
-def individual_services_content(url: str):
-    pass
+
+def individual_services_content(url: str) -> list[dict]:
+
+    html = fetch_page(url)
+    scriped_data = []
+
+    if html is None:
+        logging.error(f"No HTML content for URL: {url}")
+        return {}
+    soup = BeautifulSoup(html, "lxml")
+
+    try:
+        if url.endswith("removeit"):  # change the if condition
+            main_title = get_title(url, soup)
+
+            green_selections = get_green_titles(url, soup)
+
+            for section in green_selections:
+                combined_title = f"{main_title} - {section['title']}"
+
+                new_row = {"url": url, "title": combined_title, "text": section["text"]}
+
+                scriped_data.append(new_row)
+            df = pd.DataFrame(scriped_data)
+            df.to_csv("outputs.csv")
+
+        elif url.endswith("/733.html"):
+            df = pd.DataFrame(get_esdad_content(url, soup)) 
+            df.to_csv('test.csv')
+
+    except TitleNotFoundError as e:
+        print(e)
+        return {}
+
+    return scriped_data
+
+
+def get_esdad_content(url: str, soup: BeautifulSoup) -> list[dict]:
+    data = []
+    faq_tags = soup.select("div.faq-item")
+    title_page = get_title(url,soup)
+
+    if faq_tags:
+        for tag in faq_tags:
+
+            tilte_tag = tag.find("div", class_="faq-title")
+            title = tilte_tag.get_text(strip=True) if tilte_tag else "N/A"
+            
+            full_title = f"{title_page} - {title}"
+
+            content_points = []
+            content_tags = tag.find("div", class_="faq-content")
+
+            if content_tags:
+                content_points = [
+                    li.get_text(strip=True) for li in content_tags.find_all("li")
+                ]
+
+            full_content = "\n".join(content_points)
+            
+            row = {"url": url, 
+                   "title": full_title,
+                   "text": full_content}
+            data.append(row)
+        
+        return data
+
 
 def get_data(url: str, soup: BeautifulSoup):
     title = get_title(url, soup)
@@ -212,55 +284,63 @@ def get_data(url: str, soup: BeautifulSoup):
     return [text], [title]
 
 
-
 def extract_content(url: str):
-    individual_services_urls = ['https://aib.ps/content/accounts',
-                                'https://aib.ps/content/fund',
-                                'https://aib.ps/content/cards',
-                                'https://aib.ps/content/e-services',
-                                'https://aib.ps/content/transfers',
-                                'https://aib.ps/content/treasury-services',
-                                'https://aib.ps/content/others']
-    
-    
-    
+
+    data = []
+
+    link = "https://aib.ps/content/e-services"
+    individual_services_urls = [link]
+    # https://aib.ps/content/accounts'
+    #  'https://aib.ps/content/fund',
+    #                             'https://aib.ps/content/cards',
+    #                             'https://aib.ps/content/e-services',
+    #                             'https://aib.ps/content/transfers',
+    #                             'https://aib.ps/content/treasury-services',
+    #                             'https://aib.ps/content/others'
+
     html = fetch_page(url)
-    
+
     if html is None:
         logging.error(f"No HTML content to extract for URL: {url}")
         return
-    
-    
+
     if url.endswith("/about-us"):
         soup = BeautifulSoup(html, "lxml")
         links = soup.select("div.item-list-w a.item-list")
-        
+
         for link in links:
             href = link.get("href")
             if href:
                 full_url = urljoin(BASE_URL, str(href))
                 dic = about_us_content(full_url)
-                
+
     elif url.endswith("/individual-services"):
-        
+
         for url in individual_services_urls:
-            
+
             html = fetch_page(url)
-            soup = BeautifulSoup(html,'lxml')
-            links = soup.select("div.item-list-w a.item-list")
-        
+            soup = BeautifulSoup(html, "lxml")
+
+            if url.endswith("/e-services"):
+                links = soup.select("div.col-md-4 a.cat-list")
+            else:
+                links = soup.select("div.item-list-w a.item-list")
+
             for link in links:
                 href = link.get("href")
+                print(href)
                 if href:
                     full_url = urljoin(BASE_URL, str(href))
-                    dic = individual_services_content(full_url)
-            
-              
+                    data.extend(individual_services_content(full_url))
+                    df = pd.DataFrame(data)
+                    df.to_csv("output.csv")
+
+                else:
+                    print("error")
 
 
 def main():
-    html = fetch_page(urljoin(BASE_URL, "content/about-us"))
-    extract_content(urljoin(BASE_URL, "content/about-us"))
+    extract_content(urljoin(BASE_URL, "/content/individual-services"))
 
 
 if __name__ == "__main__":
